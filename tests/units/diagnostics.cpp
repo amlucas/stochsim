4323366e3c34c5d8ba5d702bdde0db30a8b57ea9
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2022-08-11 15:47:02 -0400

    add additional test

--- tests/units/diagnostics.cpp
+++ tests/units/diagnostics.cpp
@@ -22,3 +22,25 @@ TEST( mean_trajectory_diagnostic, mean_one_sample_is_sample )
 
     ASSERT_EQ(ss.str(),"time,A\n0,56\n");
 }
+
+TEST( mean_trajectory_diagnostic, multiple_bins_and_species )
+{
+    std::vector<std::string> speciesNames = {"A", "B"};
+    const real tend = 1.0;
+    const int numBins = 4;
+    MeanTrajectoryDiagnostic d(speciesNames, tend, numBins);
+
+    std::vector<int> state;
+
+    state = {2, 3};
+    d.collect(0, 0.1_r, state);
+    d.collect(0, 0.3_r, state);
+
+    state = {4, 5};
+    d.collect(0, 0.1_r, state);
+
+    std::stringstream ss;
+    d.dump(ss);
+
+    ASSERT_EQ(ss.str(),"time,A,B\n0,3,4\n0.25,2,3\n0.5,0,0\n0.75,0,0\n");
+}
