b0b1199766e3cafceb0c962beb581f0ae93cd3ad
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2022-08-11 11:58:34 -0400

    add one more test

--- tests/units/reaction_parser.cpp
+++ tests/units/reaction_parser.cpp
@@ -31,3 +31,21 @@ TEST( reaction_parser, supports_spaces )
     ASSERT_EQ(reactantsSCs[1], 1);
     ASSERT_EQ(productsSCs[0], 3);
 }
+
+TEST( reaction_parser, complex )
+{
+    auto [reactants, reactantsSCs, products, productsSCs] = parseReactionString("A + 45 B + 7C-> 3 C +67D");
+
+    ASSERT_EQ(reactants.size(), 3);
+    ASSERT_EQ(products.size(), 2);
+    ASSERT_EQ(reactants[0], "A");
+    ASSERT_EQ(reactants[1], "B");
+    ASSERT_EQ(reactants[2], "C");
+    ASSERT_EQ(products[0], "C");
+    ASSERT_EQ(products[1], "D");
+    ASSERT_EQ(reactantsSCs[0], 1);
+    ASSERT_EQ(reactantsSCs[1], 45);
+    ASSERT_EQ(reactantsSCs[2], 7);
+    ASSERT_EQ(productsSCs[0], 3);
+    ASSERT_EQ(productsSCs[1], 67);
+}
