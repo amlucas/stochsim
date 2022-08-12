9713582d8ced6bac3b9586f843a8e4e16cdc049b
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2022-08-12 09:31:29 -0400

    add opyion to show only a few variables

--- tools/plot_mean_trajectories.py
+++ tools/plot_mean_trajectories.py
@@ -8,7 +8,10 @@ import pandas as pd
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("csv", type=str, help="csv file to plot.")
+    parser.add_argument("--show-only", type=str, nargs='+', default=None, help="if set, show only these variables.")
     args = parser.parse_args()
+
+    to_show = args.show_only
 
     df = pd.read_csv(args.csv)
     time = df["time"].to_numpy()
@@ -18,8 +21,10 @@ if __name__ == '__main__':
     for key in df.columns:
         if key == "time":
             continue
-        x = df[key].to_numpy()
-        ax.plot(time, x, label=key)
+
+        if to_show is None or key in to_show:
+            x = df[key].to_numpy()
+            ax.plot(time, x, label=key)
 
     ax.set_xlabel(r"$t$")
     ax.set_ylabel(r"$x$")
