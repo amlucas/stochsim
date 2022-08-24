8e9f4eac360c510957300c76f4139244fd095722
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2022-08-24 15:20:35 -0400

    cleanup outdated test

--- tests/units/factory.cpp
+++ tests/units/factory.cpp
@@ -7,27 +7,6 @@
 
 using namespace ssm;
 
-TEST( factory_reaction, from_json )
-{
-    // reaction A + B -> 2 C
-    const auto config = factory::Json::parse(R"(
-{
-    "rate": 0.1,
-    "reactants_ids": [0, 1],
-    "reactants_scs": [1, 1],
-    "products_ids": [2],
-    "products_scs": [2]
-}
-)");
-
-    const Reaction reaction = factory::createReaction(config);
-
-    const int numSpecies[] = {4, 5, 10};
-    ASSERT_EQ(reaction.computePropensity(numSpecies),
-              0.1 * numSpecies[0] * numSpecies[1]);
-}
-
-
 TEST( factory_simulation, from_json_ssa )
 {
     const auto config = factory::Json::parse(R"(
