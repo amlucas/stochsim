85a0e4b2a4f20c4a27ef977d3041f77c2af32785
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2022-08-25 11:44:55 -0400

    add a python unit test for solver

---
+++ tests/python/units/test_solver.py
@@ -0,0 +1,13 @@
+import unittest
+import pyssm as ssm
+
+class TestSolver(unittest.TestCase):
+
+    def test_decays_species_goes_to_zero(self):
+        p = ssm.Problem(100, {"A": 10, "B": 0})
+        p.add_reaction(1000, "A->2B")
+        s = ssm.Solver.SSA(p)
+        t, state = ssm.run_trajectory(solver=s, problem=p)
+
+        self.assertEqual(state["A"][-1], 0)
+        self.assertEqual(state["B"][-1], 20)
